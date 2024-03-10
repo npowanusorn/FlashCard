fc1f020d4ef51c0c81c088d6ea41cc581933e600
Nucha Powanusorn <nucha.powanusorn@gmail.com>
2024-03-09 19:44:33 -0800

    classes

--- FlashCard/JSON.swift
+++ FlashCard/JSON.swift
@@ -7,12 +7,26 @@
 
 import Foundation
 
-struct Chapter: Codable, Equatable {
+class Chapter: Codable, Equatable {
+    static func == (lhs: Chapter, rhs: Chapter) -> Bool {
+        return lhs.title == rhs.title && lhs.wordList == rhs.wordList
+    }
+    
     var title: String
     var wordList: [WordList]
 }
 
-struct WordList: Codable, Equatable {
+class WordList: Codable, Equatable {
+    static func == (lhs: WordList, rhs: WordList) -> Bool {
+        return (lhs.korDef == rhs.korDef) &&
+        (lhs.enDef == rhs.enDef) && 
+        (lhs.syn == rhs.syn) &&
+        (lhs.ant == rhs.ant) &&
+        (lhs.korExSe == rhs.korExSe) &&
+        (lhs.enExSe == rhs.enExSe) &&
+        (lhs.descr == rhs.descr)
+    }
+    
     var korDef: String
     var enDef: String
     var syn: String
